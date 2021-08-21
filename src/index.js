a8deb36fc45defa6bb0cea82f73df0cbbe4e0d3c
Alexandra <alexandrastanjic@gmail.com>
2021-08-21 18:00:46 +0300

    finish the app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-
+import { FavoritesContextProvider } from './store/favorites-context';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FavoritesContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </FavoritesContextProvider>,
   document.getElementById('root')
 );
 
